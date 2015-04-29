ed86ae19bbb1b80168fee09b87d7d0c0cb45534c
Sam Hutchings <sam@concurrentdevelopment.co.uk>
2015-04-29 21:06:53 +0100

    more register view updates

--- app/src/main/java/com/vibes/Register.java
+++ app/src/main/java/com/vibes/Register.java
@@ -36,16 +36,9 @@ import java.util.List;
 public class Register extends Activity implements LoaderCallbacks<Cursor> {
 
     /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
-    /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
-    private UserLoginTask mAuthTask = null;
+    private UserRegisterTask mAuthTask = null;
 
     // UI references.
     private AutoCompleteTextView mEmailView;
@@ -244,14 +237,12 @@ public class Register extends Activity implements LoaderCallbacks<Cursor> {
      * Represents an asynchronous login/registration task used to authenticate
      * the user.
      */
-    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+    public class UserRegisterTask extends AsyncTask<Void, Void, Boolean> {
 
-        private final String mEmail;
-        private final String mPassword;
+        private final String mPhoneNumber;
 
-        UserLoginTask(String email, String password) {
-            mEmail = email;
-            mPassword = password;
+        UserRegisterTask(String phoneNumber) {
+            mPhoneNumber = phoneNumber;
         }
 
         @Override
@@ -263,14 +254,6 @@ public class Register extends Activity implements LoaderCallbacks<Cursor> {
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
                 return false;
-            }
-
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
             }
 
             // TODO: register the new account here.
