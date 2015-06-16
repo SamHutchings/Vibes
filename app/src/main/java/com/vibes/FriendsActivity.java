62ad67a530e2dfbcd802f8496a9da9876d7d0394
Sam Hutchings <sam@concurrentdevelopment.co.uk>
2015-06-16 21:02:31 +0100

    more activities. couple more strings

--- app/src/main/java/com/vibes/FriendsActivity.java
+++ app/src/main/java/com/vibes/FriendsActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 
 import com.vibes.data.ContactsDataSource;
 import com.vibes.data.VibesDataSource;
+import com.vibes.services.DisconnectedVibesAPI;
 
 /**
  * Activity for the friends page
@@ -11,4 +12,5 @@ import com.vibes.data.VibesDataSource;
 public class FriendsActivity extends Activity {
     private VibesDataSource vibesDataSource;
     private ContactsDataSource contactsDataSource;
+    private DisconnectedVibesAPI api;
 }
